92a9e09ed2c01013578e0c1920b33968c11f059f
jbrolex42-cell <jbrolex42@gmail.com>
2026-09-23 14:02:12 +0300

    Add production LibreTranslate service

---
+++ wsgi.py
@@ -0,0 +1,3 @@
+from libretranslate.app import create_appapp = create_app()
+
+from libretranslate.app import create_appfrom libretranslate.default_values import DEFAULT_ARGUMENTSclass Args:    def __init__(self):        for key, value in DEFAULT_ARGUMENTS.items():            setattr(self, key, value)        self.host = "0.0.0.0"        self.port = 10000        self.load_only = [            "en",            "sw",            "fr",            "es",            "de",            "pt",            "it",            "ar",            "hi",            "zh",            "ja",            "ko",            "ru",            "nl",            "tr",        ]app = create_app(Args())
