fa7b62a3c3f49197d118dfe0c27880f15a675f35
jbrolex42-cell <jbrolex42@gmail.com>
2026-09-23 14:25:44 +0300

    Fix LibreTranslate application arguments

--- wsgi.py
+++ wsgi.py
@@ -4,15 +4,12 @@ from libretranslate.default_values import DEFAULT_ARGUMENTS
 
 class Args:
     def __init__(self):
-        # Load every LibreTranslate default argument.
         for key, value in DEFAULT_ARGUMENTS.items():
             setattr(self, key, value)
 
-        # Render's HTTP server settings.
         self.host = "0.0.0.0"
         self.port = 10000
 
-        # Languages to make available.
         self.load_only = [
             "en",
             "sw",
@@ -30,6 +27,10 @@ class Args:
             "nl",
             "tr",
         ]
+
+        # Required by LibreTranslate's create_app().
+        self.update_models = False
+        self.force_update_models = False
 
 
 app = create_app(Args())
